a9f3f5b31aeac652c7e7ab10030a287dbcd50821
Stephen Celis <stephen@stephencelis.com>
2013-12-26 18:48:06 -0800

    Initial Commit

---
+++ BRLOptionParser/BRLOptionParser.h
@@ -0,0 +1,13 @@
+//
+//  BRLOptionParser.h
+//  BRLOptionParser
+//
+//  Created by Stephen Celis on 12/26/13.
+//  Copyright (c) 2013 Barrel Age. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRLOptionParser : NSObject
+
+@end
---
+++ BRLOptionParser/BRLOptionParser.m
@@ -0,0 +1,13 @@
+//
+//  BRLOptionParser.m
+//  BRLOptionParser
+//
+//  Created by Stephen Celis on 12/26/13.
+//  Copyright (c) 2013 Barrel Age. All rights reserved.
+//
+
+#import "BRLOptionParser.h"
+
+@implementation BRLOptionParser
+
+@end
